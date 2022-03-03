d87ed4fd66d155e2300280b7b0f9352094b82e4f
Olexii Yefimenko <o.yefimenko@zakaz.global>
2022-03-03 19:29:16 +0200

    Base application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
+import "./styles/App.css";
+
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+    { id: 4, title: 'Javascript 4', body: 'Description' },
+    { id: 5, title: 'Javascript 5', body: 'Description' },
+  ])
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    } else {
+      return posts
+    }
+  }, [selectedSort, posts])
+
+  const sortedSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  }, [searchQuery, sortedPosts])
+
+  const createPost = (newPost) => {
+    if (newPost.title && newPost.body)
+      setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MyInput 
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Search..."
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortBy => setSelectedSort(sortBy)}
+          defaultValue="Sort by"
+          options={[
+            {value: 'title', name: 'by name'},
+            {value: 'body', name: 'by description'},
+          ]}
+        />
+      </div>
+      {sortedSearchedPosts.length !== 0
+        ?
+        <PostList posts={sortedSearchedPosts} remove={removePost} title="List of posts" />
+        :
+        <h1 style={{ textAlign: 'center' }}>Posts not found</h1>
+      }
     </div>
   );
 }
